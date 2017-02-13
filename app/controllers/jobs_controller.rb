e54e04b60cb929fa39c1b2e1b0f9408a533ab2da
Bruno e Henrique <henriquepjv@gmail.com>
2017-02-13 20:37:00 -0200

    cadastro empresas final

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,18 +7,18 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
   def new
+    @companies = Company.all
     @job = Job.new
   end
   def create
-    #adicionar codigo reduzido
-    @job = Job.new
-    @job.title = params[:job][:title]
-    @job.location = params[:job][:location]
-    @job.category = params[:job][:category]
-    @job.company = params[:job][:company]
-    @job.description = params[:job][:description]
-    @job.featured = params[:job][:featured]
+    @job = Job.new(job_params)
     @job.save
     redirect_to job_url(@job.id)
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :location, :category, :company_id, :description, :featured)
   end
 end
