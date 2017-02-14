f6f801ea10dab99f11c4198846bfd6662acd8b67
Bruno e Henrique <henriquepjv@gmail.com>
2017-02-13 22:11:45 -0200

    criar empresa

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,19 @@
+class CompaniesController <  ApplicationController
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    @company.save
+      redirect_to @company
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :location, :mail, :phone)
+  end
+
+end
